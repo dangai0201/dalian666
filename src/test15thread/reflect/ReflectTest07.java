a2a73fb78c5a5a21f9c2564947c3381eb6ff30be
yuliguo <1471375117@qq.com>
2022-08-08 23:28:59 +0800

    更新代码

--- src/test15thread/reflect/ReflectTest07.java
+++ src/test15thread/reflect/ReflectTest07.java
@@ -12,6 +12,7 @@ public class ReflectTest07 {
     public static void main(String[] args) throws  Exception{
         //创建这个是为了拼接字符串
         StringBuilder s=new StringBuilder();
+
 //        Class studentClass=Class.forName("test15thread.reflect.Student");
         Class studentClass=Class.forName("java.util.Date");
 
